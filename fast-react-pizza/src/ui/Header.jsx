4b66e5916e2db39d4b1f00841d9c8c78082e5da1
arijitk930 <arijitk930@gmail.com>
2025-07-12 15:56:04 +0530

    adding styles

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -3,11 +3,13 @@ import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
-    <div>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza Co.</Link>
+
       <SearchOrder />
+
       <p>Jonas</p>
-    </div>
+    </header>
   );
 }
 
