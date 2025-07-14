59a26cdef854f04f9ecc19bf40dfa4c6b4a1873f
arijitk930 <arijitk930@gmail.com>
2025-07-14 22:03:42 +0530

    308. Styling the Menu

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/Username";
 function Header() {
   return (
     <header className="flex items-center justify-between bg-yellow-500 uppercase px-4 py-3 border-b border-stone-300 sm:px-6">
-      <Link to="/" className="tracking-widest">
+      <Link to="/" className="tracking-widest font-pizza">
         Fast React Pizza Co.
       </Link>
 
